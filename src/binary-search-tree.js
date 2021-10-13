746a9dacbf4583e35f4410ce2570f64e50e4b4c8
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-10-14 01:43:42 +0500

    feat: add method has for BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,64 +1,77 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 module.exports = class BinarySearchTree {
-  constructor() {
-    this.startData = null;
-  }
+    constructor() {
+        this.startData = null;
+    }
 
-  root() {
-    return this.startData;
-  }
+    root() {
+        return this.startData;
+    }
 
-  add(data) {
-    this.startData = addWithin(this.startData, data);
+    add(data) {
+        this.startData = addWithin(this.startData, data);
 
-    function addWithin(node, data) {
-        if(!node) {
-            return new Node(data);
-        }
-        
-        if (node.data === data) {
+        function addWithin(node, data) {
+            if (!node) {
+                return new Node(data);
+            }
+
+            if (node.data === data) {
+                return node;
+            }
+
+            if (data < node.data) {
+                node.left = addWithin(node.left, data);
+            } else {
+                node.right = addWithin(node.right, data);
+            }
+
             return node;
         }
-
-        if (data < node.data) {
-            node.left = addWithin(node.left, data)
-        } else {
-            node.right = addWithin(node.right, data)
-        }
-
-        return node;
     }
-}
 
-  has(data) {
+    has(data) {
+        return serch(this.startData, data);
 
-  }
+        function serch(node, data) {
+            if (!node) {
+                return false;
+            }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+            if (node.data === data) {
+                return true;
+            }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+            return data < node.data
+                ? serch(node.left, data)
+                : serch(node.right, data);
+        }
+    }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    find(/* data */) {
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    remove(/* data */) {
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    }
 
-}
+    min() {
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    }
+
+    max() {
+        throw new NotImplementedError("Not implemented");
+        // remove line with error and write your code here
+    }
+};
