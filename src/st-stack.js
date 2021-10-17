6901b8ee840ecd9dfc317e7cdef580b894dafd0f
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-10-17 11:05:10 +0500

    feat: add st-stack (Вс, окт 17, 2021 11:05:10)

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,28 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 module.exports = class Stack {
-    push(/* element */) {
-        throw new NotImplementedError("Not implemented");
-        // remove line with error and write your code here
+    constructor() {
+        this.items = [];
+        this.count = 0;
+    }
+
+    push(el) {
+        this.items[this.count] = el;
+        console.log(`${el} add to ${this.count}`)
+        this.count += 1;
+        return this.count - 1;
     }
 
     pop() {
-        throw new NotImplementedError("Not implemented");
-        // remove line with error and write your code here
+        if(this.count == 0) return undefined;
+        let deletItem = this.items[this.count - 1];
+        this.count -= 1;
+        console.log(`${deletItem} removed`)
+        return deletItem;
     }
 
     peek() {
-        throw new NotImplementedError("Not implemented");
-        // remove line with error and write your code here
+        console.log(`Top is ${this.items[this.count - 1]}`)
+        return this.items[this.count - 1];
     }
 };
