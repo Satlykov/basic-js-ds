fbec7afe469a4016e007b871a2a33890f65d9464
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-10-17 10:31:12 +0500

    feat: add st-remove-form-list (Вс, окт 17, 2021 10:31:11)

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList( l, k ) {
+
+    function lengthList(list) {
+        let i = 0;
+        let cur = list;
+    
+        while (cur) {
+            i++;
+            if (cur.next === null) {
+                return i;
+            }
+    
+            cur = cur.next;
+        }
+    }
+
+    function indexOf(el, list) {
+        let cur = list;
+        let i = 0;
+    
+        while (cur) {
+            if (cur.value === el) {
+                return i;
+            }
+    
+            cur = cur.next;
+            i++;
+        }
+    
+        return -1;
+    }
+
+    function removeAt(pos, l) {
+        if (pos < 0 || pos >= lengthList(l)) {
+            return;
+        }
+    
+        let cur = l;
+    
+        if (pos === 0) {
+            return l = cur.next;
+        } else if(pos === 1){
+            cur = cur.next.next
+            l.next = cur;
+            return l;
+        } else if(pos === 2){
+            cur = cur.next.next.next
+            l.next.next = cur;
+            return l;
+        } else if(pos === 3){
+            cur = cur.next.next.next.next
+            l.next.next.next = cur;
+            return l;
+        } else if(pos === 4){
+            cur = cur.next.next.next.next.next
+            l.next.next.next.next = cur;
+            return l;
+        }
+    
+        return l;
+    }
+
+    return removeAt(indexOf(k, removeAt(indexOf(k, l), l)), removeAt(indexOf(k, l), l))
 }
